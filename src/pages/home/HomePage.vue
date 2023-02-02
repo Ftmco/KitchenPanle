c1c3f99b163deb38ef78b9f9265cfb38c6299bba
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-02 22:37:13 +0330

    Update HomePage.vue

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,13 +1,18 @@
 <template>
-  <v-col cols="12"> </v-col>
+  <div>
+    <weekly-plan />
+    <inventory-alert />
+  </div>
 </template>
 
 <script lang="ts">
 import AppSparkline from "@/components/core/AppSparkline.vue";
+import InventoryAlert from "@/components/home/inventoryAlert.vue";
+import WeeklyPlan from "@/components/home/weeklyPlan.vue";
 import router from "@/router";
 import Vue from "vue";
 export default Vue.extend({
-  components: { AppSparkline },
+  components: { AppSparkline, WeeklyPlan, InventoryAlert },
 
   data: () => ({}),
 });
