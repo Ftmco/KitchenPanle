2efe00933708d6c7fdeb01c100f1c4ce141d6dd3
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-04 12:39:52 +0330

    create base url

--- src/api/urls/index.ts
+++ src/api/urls/index.ts
@@ -1,4 +1,4 @@
-export const baseURLs = (isPublic: boolean) => '';
+export const baseURLs = (isPublic: boolean) => 'http://localhost:5293/api';
 
 export const group = {
     groups: (page: number, count: number) => `/Group/Groups?page=${page}&count=${count}`,
