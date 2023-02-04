9f7aa11a8fedd90a43da6e7a7cf81c6f73a3f762
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-04 22:35:42 +0330

    Update TypesPage.vue

--- src/pages/home/TypesPage.vue
+++ src/pages/home/TypesPage.vue
@@ -4,8 +4,8 @@
       <table-header
         title="نوع ها"
         newTitle="نوع جدید"
-        :newAction="() => {}"
-        :reloadAction="() => {}"
+        :newAction="addType"
+        :reloadAction="loadTypes"
       >
         <template v-slot:search>
           <v-text-field
@@ -33,13 +33,13 @@
         <template v-slot:item.actions="{ item }">
           <v-row>
             <v-col>
-              <v-btn block color="warning" text>
+              <v-btn block color="warning" text @click="editType(item)">
                 ویرایش
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
             </v-col>
             <v-col>
-              <v-btn block color="error" text>
+              <v-btn block color="error" text @click="removeType(item)">
                 حذف
                 <v-icon>mdi-delete</v-icon>
               </v-btn>
@@ -55,11 +55,22 @@
 </template>
 
 <script lang="ts">
+import { deleteType, getTypes } from "@/api/apis/type.api";
 import TableHeader from "@/components/core/TableHeader.vue";
-import { TableHeaderModel } from "@/components/models";
+import { ConfirmDialog, Dialog, TableHeaderModel } from "@/components/models";
 import { DIALOG, SNACKBAR } from "@/store/store_types";
 import Vue from "vue";
-import { mapMutations } from "vuex";
+import {
+  VCol,
+  VCard,
+  VTextField,
+  VDataTable,
+  VRow,
+  VBtn,
+  VIcon,
+  VPagination,
+} from "vuetify/lib";
+import types, { mapMutations, mapState } from "vuex";
 export default Vue.extend({
   components: { TableHeader },
   data: () => ({
@@ -72,7 +83,7 @@ export default Vue.extend({
         text: "عنوان",
         align: "start",
         sortable: true,
-        value: "title",
+        value: "name",
       },
       {
         text: "تاریخ ایجاد",
@@ -89,11 +100,87 @@ export default Vue.extend({
     ] as Array<TableHeaderModel>,
     types: [] as Array<any>,
   }),
-  mounted() {},
+  computed: {
+    ...mapState(DIALOG, {
+      dialogResult: `dialogResult`,
+      confirmDialogResult: `confirmDialogResult`,
+    }),
+  },
+  watch: {
+    dialogResult(result) {
+      if (result != undefined && result.status) {
+        this.findAndRemoveType(result.data.id);
+        this.types.push(result.data);
+      }
+    },
+    confirmDialogResult(result) {
+      if (result.agree) this.deleteConfirm(result.data);
+    },
+  },
+  mounted() {
+    this.loadTypes();
+  },
   methods: {
-    ...mapMutations(DIALOG, ["showModal", "hideModal"]),
+    ...mapMutations(DIALOG, ["showModal", "hideModal", "showConfirm"]),
     ...mapMutations(SNACKBAR, ["showSnackbar"]),
-    loadTypes() {},
+    loadTypes() {
+      getTypes(0, 20)
+        .then((typesRes) => {
+          if (typesRes.status) {
+            this.pageCount = typesRes.result.pageCount;
+            this.types = typesRes.result.types;
+          }
+        })
+        .finally(() => (this.isLoading = false));
+    },
+    addType() {
+      const create: Dialog = {
+        color: "primary",
+        title: "ایجاد نوع جدید",
+        content: {
+          component: () => import("@/components/type/Upsert.vue"),
+          props: {},
+        },
+      };
+      this.showModal(create);
+    },
+    editType(item: any) {
+      const update: Dialog = {
+        color: "warning",
+        title: `ویرایش ${item.name}`,
+        content: {
+          component: () => import("@/components/type/Upsert.vue"),
+          props: {
+            updateType: item,
+          },
+        },
+      };
+      this.showModal(update);
+    },
+    findAndRemoveType(id: string) {
+      let index = this.types.findIndex((type) => type.id == id);
+      if (index != -1) {
+        this.types.splice(index, 1);
+      }
+    },
+    removeType(item: any) {
+      const confirm: ConfirmDialog = {
+        data: item.id,
+        text: `آیا از حذف ${item.name} مطمئن هستید؟`,
+        title: "حذف گروه",
+        agreeText: "بله",
+        disagreeText: "لغو",
+      };
+      this.showConfirm(confirm);
+    },
+    deleteConfirm(data: any) {
+      deleteType(data).then((delRes) => {
+        if (delRes) {
+          this.findAndRemoveType(data);
+        }
+        this.showSnackbar(delRes.title);
+      });
+    },
   },
 });
 </script>
