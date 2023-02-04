d2bb074661a0f0c18d054c8eec2435759d260c8a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-04 12:39:58 +0330

    Update GroupsPage.vue

--- src/pages/home/GroupsPage.vue
+++ src/pages/home/GroupsPage.vue
@@ -25,11 +25,16 @@
         :headers="headers"
         :items="groups"
         :search="search"
-        no-data-text="نظری یافت نشد"
+        no-data-text="گروهی یافت نشد"
         loading-text="کمی صبر کنید..."
         no-results-text="موردی یافت نشد"
         hide-default-footer
       >
+        <template v-slot:item.status="{ item }">
+          <v-chip :color="getBaseStatusObj(item.status).color">
+            {{ getBaseStatusObj(item.status).title }}
+          </v-chip>
+        </template>
         <template v-slot:item.actions="{ item }">
           <v-row>
             <v-col>
@@ -58,6 +63,7 @@
 import { deleteGroup, getGroups } from "@/api/apis/group.apis";
 import TableHeader from "@/components/core/TableHeader.vue";
 import { ConfirmDialog, Dialog, TableHeaderModel } from "@/components/models";
+import { getBaseStatusObj } from "@/services/status";
 import { DIALOG, SNACKBAR } from "@/store/store_types";
 import Vue from "vue";
 import { mapMutations, mapState } from "vuex";
@@ -67,26 +73,20 @@ export default Vue.extend({
     inLoading: true,
     headers: [
       {
-        text: "آواتار",
-        value: "avatar",
-        align: "start",
-        sortable: false,
-      },
-      {
         text: "نام",
         value: "name",
         align: "start",
         sortable: true,
       },
       {
-        text: "عنوان",
-        value: "title",
+        text: "وضیعت",
+        value: "status",
         align: "start",
         sortable: true,
       },
       {
-        text: "کد",
-        value: "code",
+        text: "تاریخ ایجاد",
+        value: "createDate",
         align: "start",
         sortable: true,
       },
@@ -111,8 +111,8 @@ export default Vue.extend({
   watch: {
     dialogResult(result) {
       if (result != undefined && result.status) {
-        this.findAndRemoveGroup(result.data.group.id);
-        this.groups.push(result.data.group);
+        this.findAndRemoveGroup(result.data.id, true);
+        this.groups.push(result.data);
       }
     },
     confirmDialogResult(result) {
@@ -163,7 +163,7 @@ export default Vue.extend({
     removeGroup(item: any) {
       const confirm: ConfirmDialog = {
         data: item.id,
-        text: `آیا از حذف ${item.title} مطمئن هستید؟`,
+        text: `آیا از حذف ${item.name} مطمئن هستید؟`,
         title: "حذف گروه",
         agreeText: "بله",
         disagreeText: "لغو",
@@ -172,14 +172,19 @@ export default Vue.extend({
     },
     deleteConfirm(id: string) {
       deleteGroup(id).then((deleteRes) => {
-        if (deleteRes.status) this.findAndRemoveGroup(id);
+        if (deleteRes.status) this.findAndRemoveGroup(id, false);
         this.showSnackbar(deleteRes.title);
       });
     },
-    findAndRemoveGroup(id: string) {
+    findAndRemoveGroup(id: string, remove: boolean) {
       let index = this.groups.findIndex((g) => g.id == id);
-      if (index != -1) this.groups.splice(index, 1);
+      if (index != -1) {
+        let group = this.groups[index];
+        this.groups.splice(index, 1);
+        if (!remove) this.groups.push({ ...group, status: 2 });
+      }
     },
+    getBaseStatusObj,
   },
 });
 </script>
