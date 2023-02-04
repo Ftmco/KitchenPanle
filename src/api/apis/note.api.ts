5dac2bdae34ab7f946cf37fa19b9d00c6a4291e9
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-04 14:24:26 +0330

    upsert and delete note

--- src/api/apis/note.api.ts
+++ src/api/apis/note.api.ts
@@ -1,5 +1,6 @@
 import { apiCall } from "..";
 import { ApiResponse } from "../models/api.model";
+import { UpsertNote } from "../models/note.model";
 import { baseURLs, note } from "../urls";
 
 const axios = apiCall(baseURLs(false));
@@ -8,6 +9,28 @@ export const getNotes = (page: number, count: number): Promise<ApiResponse> =>
     new Promise(async (resolve, reject) => {
         try {
             const request = await axios.get(note.notes(page, count))
+            const response = await request.data
+            resolve(response as ApiResponse)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+export const upsertNote = (upsert: UpsertNote): Promise<ApiResponse> =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const request = await axios.post(note.upsert, upsert)
+            const response = await request.data
+            resolve(response as ApiResponse)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+export const deleteNote = (id: string): Promise<ApiResponse> =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const request = await axios.delete(note.delete(id))
             const response = await request.data
             resolve(response as ApiResponse)
         } catch (error) {
