cfa3aac952a9ed280d3564a4ad242dcb5e8417ac
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-06 19:35:40 +0330

    Update FoodsPage.vue

--- src/pages/home/FoodsPage.vue
+++ src/pages/home/FoodsPage.vue
@@ -4,8 +4,8 @@
       <table-header
         title="غذا ها"
         newTitle="غذای جدید"
-        :newAction="() => {}"
-        :reloadAction="() => {}"
+        :newAction="addFood"
+        :reloadAction="loadFoods"
       >
         <template v-slot:search>
           <v-text-field
@@ -30,16 +30,40 @@
         no-results-text="موردی یافت نشد"
         hide-default-footer
       >
+        <template v-slot:item.type="{ item }">
+          <v-chip :color="item.type == 0 ? 'primary' : 'success'">
+            {{ item.type == 0 ? "سرباز" : "کارمند" }}
+          </v-chip>
+        </template>
+        <template v-slot:item.status="{ item }">
+          <v-chip :color="getBaseStatusObj(item.status).color">
+            {{ getBaseStatusObj(item.status).title }}
+          </v-chip>
+        </template>
         <template v-slot:item.actions="{ item }">
           <v-row>
             <v-col>
-              <v-btn block color="warning" text>
+              <v-btn
+                block
+                color="info"
+                text
+                :to="{
+                  name: 'Norms',
+                  query: { name: item.name, foodId: item.id },
+                }"
+              >
+                مشاهده نورم
+                <v-icon>mdi-list-status</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn block color="warning" text @click="editFood(item)">
                 ویرایش
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
             </v-col>
             <v-col>
-              <v-btn block color="error" text>
+              <v-btn block color="error" text @click="removeFood(item)">
                 حذف
                 <v-icon>mdi-delete</v-icon>
               </v-btn>
@@ -55,9 +79,14 @@
 </template>
 
 <script lang="ts">
+import { deleteFood, getFoods } from "@/api/apis/food.api";
 import TableHeader from "@/components/core/TableHeader.vue";
-import { TableHeaderModel } from "@/components/models";
+import { ConfirmDialog, Dialog, TableHeaderModel } from "@/components/models";
+import router from "@/router";
+import { getBaseStatusObj } from "@/services/status";
+import { DIALOG, SNACKBAR } from "@/store/store_types";
 import Vue from "vue";
+import { mapMutations, mapState } from "vuex";
 export default Vue.extend({
   components: { TableHeader },
   data: () => ({
@@ -67,6 +96,12 @@ export default Vue.extend({
       {
         text: "نام",
         value: "name",
+        align: "start",
+        sortable: true,
+      },
+      {
+        text: "نوع غذا",
+        value: "type",
         align: "start",
         sortable: true,
       },
@@ -89,9 +124,92 @@ export default Vue.extend({
         sortable: false,
       },
     ] as Array<TableHeaderModel>,
-    foods: [],
+    foods: [] as Array<any>,
     page: 1,
     pageCount: 1,
   }),
+  computed: {
+    ...mapState(DIALOG, {
+      dialogResult: `dialogResult`,
+      confirmDialogResult: `confirmDialogResult`,
+    }),
+  },
+  watch: {
+    dialogResult(result) {
+      if (result != undefined && result.status) {
+        this.findAndRemoveFood(result.data.id);
+        this.foods.push(result.data);
+      }
+    },
+    confirmDialogResult(result) {
+      if (result.agree) this.deleteConfirm(result.data);
+    },
+  },
+  mounted() {
+    this.loadFoods();
+  },
+  methods: {
+    ...mapMutations(DIALOG, ["showModal", "showConfirm"]),
+    ...mapMutations(SNACKBAR, ["showSnackbar"]),
+    loadFoods() {
+      this.isLoading = true;
+      getFoods(0, 20)
+        .then((foodsRes) => {
+          if (foodsRes.status) {
+            this.pageCount = foodsRes.result.pageCount + 1;
+            this.foods = foodsRes.result.foods;
+          }
+        })
+        .finally(() => (this.isLoading = false));
+    },
+    addFood() {
+      const create: Dialog = {
+        color: "primary",
+        title: "افزودن غذای جدید",
+        content: {
+          component: () => import("@/components/food/Upsert.vue"),
+          props: {},
+        },
+      };
+      this.showModal(create);
+    },
+    editFood(item: any) {
+      const update: Dialog = {
+        color: "warning",
+        title: `ویرایش ${item.name}`,
+        content: {
+          component: () => import("@/components/food/Upsert.vue"),
+          props: {
+            updateFood: item,
+          },
+        },
+      };
+      this.showModal(update);
+    },
+    removeFood(item: any) {
+      const confirm: ConfirmDialog = {
+        data: item.id,
+        text: `آیا از حذف ${item.name} مطمئن هستید؟`,
+        agreeText: "حذف",
+        disagreeText: "لغو",
+        title: "حذف غذا",
+      };
+      this.showConfirm(confirm);
+    },
+    findAndRemoveFood(id: string) {
+      let index = this.foods.findIndex((f) => f.id == id);
+      if (index != -1) this.foods.splice(index, 1);
+    },
+    deleteConfirm(data: any) {
+      deleteFood(data).then((delRes) => {
+        if (delRes.status) {
+          this.findAndRemoveFood(data);
+          this.foods.push(delRes.result.food);
+        }
+        this.showSnackbar(delRes.title);
+      });
+    },
+    getBaseStatusObj,
+  },
 });
 </script>
