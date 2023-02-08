f8b7ff9aaa07e89618a5cbc9d4cf0011b3d4272e
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 15:46:12 +0330

    Update TheNavigation.vue

--- src/components/core/TheNavigation.vue
+++ src/components/core/TheNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <v-navigation-drawer app v-model="drawer" fixed temporary>
     <v-list nav dense>
-      <v-list-item-group v-model="group" active-class="text--accent-4">        
+      <v-list-item-group v-model="group" active-class="text--accent-4">
         <NavigationListItems :items="items" />
         <v-list-item @click="changeTheme">
           <v-list-item-icon>
@@ -9,23 +9,30 @@
           </v-list-item-icon>
           <v-list-item-title>{{ theme }}</v-list-item-title>
         </v-list-item>
+        <v-list-item @click="changeDirection">
+          <v-list-item-icon>
+            <v-icon>mdi-directions</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ dir }}</v-list-item-title>
+        </v-list-item>
       </v-list-item-group>
-    </v-list>   
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 
 <script lang="ts">
 import Vue from "vue";
-import {  navigationItems } from "@/constants/";
+import { navigationItems } from "@/constants/";
 import NavigationListItems from "./NavigationListItems.vue";
 
 export default Vue.extend({
   data: () => ({
-    drawer: false,   
+    drawer: false,
     items: navigationItems,
     group: null,
     theme: "روشن",
+    dir: "LTR",
     active: 0,
   }),
   created() {
@@ -34,13 +41,21 @@ export default Vue.extend({
   methods: {
     open() {
       this.drawer = true;
-    },   
+    },
     changeTheme() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
       this.theme = this.$vuetify.theme.dark ? "تاریک" : "روشن";
       localStorage.setItem(
         "theme",
         this.$vuetify.theme.dark ? "Dark" : "Ligth"
+      );
+    },
+    changeDirection() {
+      this.$vuetify.rtl = !this.$vuetify.rtl;
+      this.dir = this.$vuetify.rtl ? "RTL" : "LTR";
+      localStorage.setItem(
+        "dir",
+        this.$vuetify.rtl ? "RTL" : "LTR"
       );
     },
   },
