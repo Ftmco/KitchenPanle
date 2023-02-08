e1d3806b50f3bf34e38c4e390254db8c63932d0d
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 23:46:24 +0330

    create conversion type api calls

--- src/api/apis/type.api.ts
+++ src/api/apis/type.api.ts
@@ -1,5 +1,6 @@
 import { apiCall } from "..";
 import { ApiResponse } from "../models/api.model";
+import { TypeConvert } from "../models/inventory.model";
 import { baseURLs, type } from "../urls";
 
 const axios = apiCall(baseURLs(false))
@@ -30,6 +31,30 @@ export const deleteType = (id: any): Promise<ApiResponse> =>
     new Promise(async (resolve, reject) => {
         try {
             const request = await axios.delete(type.delete(id))
+            const response = await request.data
+            resolve(response as ApiResponse)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+// Conversions
+
+export const getTypeConversions = (id: string): Promise<ApiResponse> =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const request = await axios.get(type.conversions(id))
+            const response = await request.data
+            resolve(response as ApiResponse)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+export const addTypeConvert = (convert: TypeConvert): Promise<ApiResponse> =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const request = await axios.post(type.addConvert, convert)
             const response = await request.data
             resolve(response as ApiResponse)
         } catch (error) {
