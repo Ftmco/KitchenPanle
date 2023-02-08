c558bf38ea403e03cb8a2eb27a27144ce1959436
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 22:58:40 +0330

    Create TypeConvert.vue

---
+++ src/components/type/TypeConvert.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-col cols="12">
+    <v-card elevation="4" class="rounded-lg">
+      <table-header
+        title="تبدیل نوع ها"
+        newTitle="افزودن"
+        :hasNewAction="true"
+        :newAction="addConvert"
+        :reloadAction="() => {}"
+      >
+        <template v-slot:search>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="جستجو"
+            single-line
+            hide-details
+          ></v-text-field>
+        </template>
+      </table-header>
+    </v-card>
+    <br v-if="newConvert" />
+    <add-convert v-if="newConvert" />
+    <br />
+    <v-card elevation="4" class="rounded-lg">
+      <v-data-table
+        :loading="isLoading"
+        :headers="headers"
+        :items="converts"
+        :search="search"
+        no-data-text="نوعی یافت نشد"
+        loading-text="کمی صبر کنید..."
+        no-results-text="موردی یافت نشد"
+        hide-default-footer
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-row>
+            <v-col>
+              <v-btn block color="error" text>
+                حذف
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-col>
+</template>
+
+<script lang="ts">
+import { getTypes } from "@/api/apis/type.api";
+import { rules } from "@/constants";
+import Vue from "vue";
+import TableHeader from "../core/TableHeader.vue";
+import AddConvert from "@/components/type/AddConvert.vue";
+import { TableHeaderModel } from "../models";
+export default Vue.extend({
+  components: { TableHeader, AddConvert },
+  props: ["type"],
+  data: () => ({
+    rules: rules,
+    search: "",
+    isLoading: true,
+    headers: [
+      {
+        text: "از نوع",
+        align: "start",
+        sortable: false,
+        value: "fromType",
+      },
+      {
+        text: "به نوع",
+        align: "start",
+        sortable: false,
+        value: "toType",
+      },
+      {
+        text: "از مقدار",
+        align: "start",
+        sortable: false,
+        value: "fromValue",
+      },
+      {
+        text: "به مقدار",
+        align: "start",
+        sortable: false,
+        value: "tosValue",
+      },
+    ] as Array<TableHeaderModel>,
+    converts: [] as Array<any>,
+    newConvert: false,
+  }),
+  methods: {
+    addConvert() {
+      this.newConvert = !this.newConvert;
+    },
+  },
+});
+</script>
