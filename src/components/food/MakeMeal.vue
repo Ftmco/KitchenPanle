e3feae74a9070bb6ae33a48eada3bd24fdae965b
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 22:13:51 +0330

    Create MakeMeal.vue

---
+++ src/components/food/MakeMeal.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-col cols="12">
+    <v-form ref="mealForm">
+      <v-col cols="12">
+        <v-text-field
+          outlined
+          label="تعداد"
+          placeholder="تعداد"
+          type="number"
+          clearable
+          class="rounded-lg"
+          :rules="[rules.numberRequire]"
+        />
+      </v-col>
+      <v-col cols="12">
+        <v-btn
+          color="primary"
+          block
+          class="rounded-lg"
+          :loading="inAction"
+          @click="submitMeal"
+        >
+          ثبت غذا
+          <v-icon>mdi-check</v-icon>
+        </v-btn>
+      </v-col>
+    </v-form>
+  </v-col>
+</template>
+
+<script lang="ts">
+import { makeMeal } from "@/api/apis/dayfood.api";
+import { rules } from "@/constants";
+import { DIALOG, SNACKBAR } from "@/store/store_types";
+import Vue from "vue";
+import { mapMutations } from "vuex";
+export default Vue.extend({
+  props: ["dayFoodId"],
+  data: () => ({
+    inAction: false,
+    rules: rules,
+    count: 0,
+  }),
+  methods: {
+    ...mapMutations(DIALOG, ["hideModal"]),
+    ...mapMutations(SNACKBAR, ["showSnackbar"]),
+    submitMeal() {
+      let isValid = (this.$refs.mealForm as any).validate();
+      if (isValid) {
+        this.inAction = true;
+        makeMeal({
+          id: this.dayFoodId,
+          count: this.count,
+        })
+          .then((mealRes) => {
+            if (mealRes.status) this.hideModal();
+            this.showSnackbar(mealRes.title);
+          })
+          .finally(() => (this.inAction = false));
+      }
+    },
+  },
+});
+</script>
