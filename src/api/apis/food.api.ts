ba75925a9ddb3cc5425bd2525cd8bc06ad53a417
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 13:42:30 +0330

    add and remove norm api call

--- src/api/apis/food.api.ts
+++ src/api/apis/food.api.ts
@@ -1,6 +1,6 @@
 import { apiCall } from "..";
 import { ApiResponse } from "../models/api.model";
-import { UpsertFood } from "../models/food.model";
+import { AddNorm, UpsertFood } from "../models/food.model";
 import { baseURLs, food, norm } from "../urls";
 
 
@@ -43,6 +43,28 @@ export const getNorms = (foodId: string): Promise<ApiResponse> =>
     new Promise(async (resolve, reject) => {
         try {
             const request = await axios.get(norm.norms(foodId))
+            const response = await request.data
+            resolve(response as ApiResponse)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+export const addNorm = (add: AddNorm): Promise<ApiResponse> =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const request = await axios.post(norm.addNorm, add)
+            const response = await request.data
+            resolve(response as ApiResponse)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+export const removeNorm = (id: string): Promise<ApiResponse> =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const request = await axios.delete(norm.delete(id))
             const response = await request.data
             resolve(response as ApiResponse)
         } catch (error) {
