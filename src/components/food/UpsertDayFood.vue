31878efd9ddcac9f58f629c9a06f0332aaad1a2f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-08 21:48:55 +0330

    change name of component

--- src/components/food/UpsertDayFood.vue
+++ src/components/food/UpsertDayFood.vue
@@ -13,6 +13,7 @@
             item-value="id"
             class="rounded-lg"
             v-model="dayFood.dayId"
+            :rules="[rules.requireSelect]"
           />
         </v-col>
         <v-col cols="12" md="6" sm="6">
@@ -27,6 +28,7 @@
             class="rounded-lg"
             v-model="dayFood.foodId"
             @change="foodSelect"
+            :rules="[rules.requireSelect]"
           />
         </v-col>
         <v-col cols="12">
@@ -37,6 +39,7 @@
             clearable
             class="rounded-lg"
             v-model="dayFood.meal"
+            :rules="[rules.require]"
           />
         </v-col>
         <v-col cols="12">
@@ -54,7 +57,7 @@
         </v-col>
         <v-col cols="12">
           <v-btn
-            color="primary"
+            :color="dayFoodUpdate ? 'warning' : 'primary'"
             block
             class="rounded-lg"
             :loading="inAction"
@@ -71,11 +74,15 @@
 
 <script lang="ts">
 import { getDays } from "@/api/apis/day.api";
+import { upsertDayFood } from "@/api/apis/dayfood.api";
 import { getFoods } from "@/api/apis/food.api";
 import { UpsertDayFood } from "@/api/models/food.model";
 import { rules } from "@/constants";
+import { DIALOG, SNACKBAR } from "@/store/store_types";
 import Vue from "vue";
+import { mapMutations } from "vuex";
 export default Vue.extend({
+  props: ["dayFoodUpdate"],
   data: () => ({
     dayFood: {} as UpsertDayFood,
     days: [] as Array<any>,
@@ -97,7 +104,23 @@ export default Vue.extend({
     this.loadDays();
     this.loadFoods();
   },
+  mounted() {
+    this.setDayFood();
+  },
   methods: {
+    ...mapMutations(DIALOG, ["hideModal", "setDialogResult"]),
+    ...mapMutations(SNACKBAR, ["showSnackbar"]),
+    setDayFood() {
+      if (this.dayFoodUpdate) {
+        this.dayFood = {
+          id: this.dayFoodUpdate.id,
+          dayId: this.dayFoodUpdate.day.id,
+          foodId: this.dayFoodUpdate.food.id,
+          type: this.dayFoodUpdate.food.type,
+          meal: this.dayFoodUpdate.meal,
+        };
+      }
+    },
     loadFoods() {
       getFoods(0, 0).then((foodsRes) => {
         if (foodsRes.status) this.foods = foodsRes.result.foods;
@@ -115,7 +138,24 @@ export default Vue.extend({
         this.dayFood.type = food.type;
       }
     },
-    submiteDayFood() {},
+    submiteDayFood() {
+      let isValid = (this.$refs.dayFoodForm as any).validate();
+      if (isValid) {
+        this.inAction = true;
+        upsertDayFood(this.dayFood)
+          .then((upRes) => {
+            if (upRes.status) {
+              this.setDialogResult({
+                status: true,
+                data: upRes.result.food,
+              });
+              this.hideModal();
+            }
+            this.showSnackbar(upRes.title);
+          })
+          .finally(() => (this.inAction = false));
+      }
+    },
   },
 });
 </script>
