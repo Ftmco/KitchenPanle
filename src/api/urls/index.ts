5048a9ffad17f2a5a7a50914d646be81b19bfb27
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-10 15:24:13 +0330

    change  routes api call with pagination model

--- src/api/urls/index.ts
+++ src/api/urls/index.ts
@@ -3,19 +3,19 @@ import { Pagination } from "@/components/models";
 export const baseURLs = (isPublic: boolean) => 'http://localhost:5293/api';
 
 export const group = {
-    groups: (page: number, count: number) => `/Group/Groups?page=${page}&count=${count}`,
+    groups: (pagination: Pagination) => `/Group/Groups?page=${pagination.page}&count=${pagination.count}`,
     upsert: "/Group/Upsert",
     delete: (id: string) => `/Group/Delete?id=${id}`
 }
 
 export const note = {
-    notes: (page: number, count: number) => `/Note/Notes?page=${page}&count=${count}`,
+    notes: (pagination: Pagination) => `/Note/Notes?page=${pagination.page}&count=${pagination.count}`,
     upsert: "/Note/Upsert",
     delete: (id: string) => `/Note/Delete?id=${id}`
 }
 
 export const type = {
-    types: (page: number, count: number) => `/MeasurementType/Types?page=${page}&count=${count}`,
+    types: (pagination: Pagination) => `/MeasurementType/Types?page=${pagination.page}&count=${pagination.count}`,
     upsert: "/MeasurementType/Upsert",
     delete: (id: string) => `/MeasurementType/Delete?id=${id}`,
     conversions: (id: string) => `/MeasurementType/Conversions?id=${id}`,
@@ -23,14 +23,14 @@ export const type = {
 }
 
 export const inventory = {
-    inventories: (page: number, count: number) => `/Inventory/Inventory?page=${page}&count=${count}`,
+    inventories: (pagination: Pagination) => `/Inventory/Inventory?page=${pagination.page}&count=${pagination.count}`,
     preview: '/Inventory/Preview',
     upsert: "/Inventory/Upsert",
     delete: (id: string) => `/Inventory/Delete?id=${id}`
 }
 
 export const food = {
-    foods: (page: number, count: number) => `/Food/Foods?page=${page}&count=${count}`,
+    foods: (pagination: Pagination) => `/Food/Foods?page=${pagination.page}&count=${pagination.count}`,
     upsert: "/Food/Upsert",
     delete: (id: string) => `/Food/Delete?id=${id}`
 }
@@ -49,7 +49,7 @@ export const day = {
 
 export const dayfood = {
     daysFoods: (pagination: Pagination) => `/DayFood/DaysFoods?page=${pagination.page}&count=${pagination.count}`,
-    history:(pagination:Pagination)=> `/FoodHistory/FoodHistories?page=${pagination.page}&count=${pagination.count}`,
+    history: (pagination: Pagination) => `/FoodHistory/FoodHistories?page=${pagination.page}&count=${pagination.count}`,
     upsert: "/DayFood/Upsert",
     delete: (id: string) => `/DayFood/Delete?id=${id}`,
     makeMeal: '/DayFood/MakeMeal'
