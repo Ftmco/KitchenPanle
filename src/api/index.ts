35202125ecee926371f4b11ed972aafb4778e09f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-10 14:26:07 +0330

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,4 @@
-import Axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
-
+import Axios, { AxiosInstance, AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
 
 const _headers: AxiosRequestHeaders = {
     "Content-Type": "application/json",
@@ -9,14 +8,15 @@ const _confing: AxiosRequestConfig = {
     headers: _headers
 }
 
-export const apiCall = (baseUrl: string, headers: Array<ApiHeader> = [{ key: '', value: '' }]) => {
-    _confing.baseURL = baseUrl
-    headers.forEach((header) => {
-        if (header.key != "")
-            (_confing.headers as AxiosRequestHeaders)[header.key] = header.value
-    })
-    return Axios.create(_confing)
-}
+export const apiCall =
+    (baseUrl: string, headers: Array<ApiHeader> = [{ key: '', value: '' }]): AxiosInstance => {
+        _confing.baseURL = baseUrl
+        headers.forEach((header) => {
+            if (header.key != "")
+                _confing.headers![header.key] = header.value
+        })
+        return Axios.create(_confing)
+    }
 
 export interface ApiHeader {
     key: string
