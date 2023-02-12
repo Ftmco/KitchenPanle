eb152a019d8d6e0d7592b941f8e7574c446e1277
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-02-12 22:33:57 +0330

    Update Report.vue

--- src/pages/inventory/Report.vue
+++ src/pages/inventory/Report.vue
@@ -1,7 +1,7 @@
 <template>
   <v-col cols="12">
     <v-card elevation="4" class="rounded-lg">
-      <table-header title="گزارش غذا" :reloadAction="loadReports">
+      <table-header title="گزارش موجودی" :reloadAction="loadReports">
         <template v-slot:search>
           <v-text-field
             v-model="search"
@@ -14,16 +14,59 @@
       </table-header>
     </v-card>
     <br />
-    <v-card elevation="4" class="rounded-lg"></v-card>
+    <v-card elevation="4" class="rounded-lg">
+      <v-data-table
+        :loading="isLoading"
+        :headers="headers"
+        :items="reports"
+        :search="search"
+        no-data-text="موردی یافت نشد"
+        loading-text="کمی صبر کنید..."
+        no-results-text="موردی یافت نشد"
+        hide-default-footer
+      >
+        <template v-slot:item.count="{ item }">
+          <v-chip color="primary">
+            {{ item.historyList.length }}
+          </v-chip>
+        </template>
+
+        <template v-slot:item.type="{ item }">
+          <v-chip :color="item.type == 0 ? 'success' : 'warning'">
+            {{ item.type == 0 ? "ورود" : "خروج" }}
+          </v-chip>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-row>
+            <v-col>
+              <v-btn block color="info" text @click="showItemList(item)">
+                مشاهده موارد
+                <v-icon>mdi-list-status</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </template>
+      </v-data-table>
+      <div class="text-center">
+        <v-pagination v-model="page" :length="pageCount" @input="pageChange" />
+      </div>
+    </v-card>
   </v-col>
 </template>
 
 <script lang="ts">
 import { getHistory } from "@/api/apis/inventory.api";
 import TableHeader from "@/components/core/TableHeader.vue";
-import { defaultPage, Pagination } from "@/components/models";
+import {
+  defaultPage,
+  Dialog,
+  Pagination,
+  TableHeaderModel,
+} from "@/components/models";
 import { pageListSize } from "@/constants";
+import { DIALOG } from "@/store/store_types";
 import Vue from "vue";
+import { mapMutations } from "vuex";
 export default Vue.extend({
   components: { TableHeader },
   data: () => ({
@@ -32,11 +75,44 @@ export default Vue.extend({
     page: 1,
     pageCount: 1,
     reports: [] as Array<any>,
+    headers: [
+      {
+        text: "تاریخ",
+        align: "center",
+        sortable: true,
+        value: "createDate",
+      },
+      {
+        text: "نوع",
+        align: "center",
+        sortable: true,
+        value: "type",
+      },
+      {
+        text: "توضیحات",
+        align: "center",
+        sortable: true,
+        value: "description",
+      },
+      {
+        text: "تعداد",
+        align: "center",
+        sortable: true,
+        value: "count",
+      },
+      {
+        text: "",
+        align: "center",
+        sortable: false,
+        value: "actions",
+      },
+    ] as Array<TableHeaderModel>,
   }),
   mounted() {
     this.loadReports(defaultPage);
   },
   methods: {
+    ...mapMutations(DIALOG, ["showModal"]),
     pageChange(value: any) {
       this.loadReports({ page: value - 1, count: pageListSize });
     },
@@ -51,6 +127,19 @@ export default Vue.extend({
           }
         })
         .finally(() => (this.isLoading = false));
+    },
+    showItemList(item: any) {
+      const list: Dialog = {
+        color: "info",
+        title: `موجودی ها`,
+        content: {
+          component: () => import("@/components/inventory/HistoryList.vue"),
+          props: {
+            historyList: item.historyList,
+          },
+        },
+      };
+      this.showModal(list);
     },
   },
 });
