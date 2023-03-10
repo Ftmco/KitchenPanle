37dd1605736613d31b63331a9a1c3b9252f3b793
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2023-03-10 11:46:08 +0330

    Update search.ts

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,10 +1,8 @@
-let timeOut = 0;
+let timeOut = -1;
 
-export const searchList = (loadFunc: Function,search:string) => {
-    if (timeOut != -1) {
-        clearTimeout(timeOut);
-    }
+export const searchList = (loadFunc: Function, search: string) => {
+    clearTimeout(timeOut);
     timeOut = setTimeout(() => {
-        loadFunc({ page: 0, count: 0 }, search != "")
+        loadFunc({ page: 0, count: 0 }, search == "")
     }, 400);
 }
